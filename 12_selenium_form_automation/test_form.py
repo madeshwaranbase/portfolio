179056a04536b042de88b8642631573c440aebfe
Madeshwaran <madeshwaranponnudurai@gmail.com>
2026-09-04 19:43:41 +0530

    Refactor test_form to use pytest fixture for driver
    Refactor test_form to use pytest fixture for driver setup and teardown.

--- 12_selenium_form_automation/test_form.py
+++ 12_selenium_form_automation/test_form.py
@@ -1,21 +1,30 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-def test_form():
+@pytest.fixture
+def driver():
     driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+
+def test_checkboxes(driver):
     driver.get("https://the-internet.herokuapp.com/checkboxes")
 
-    checkboxes = driver.find_elements(By.CSS_SELECTOR, "input[type='checkbox']")
+    checkboxes = driver.find_elements(
+        By.CSS_SELECTOR, "input[type='checkbox']"
+    )
 
+    # First checkbox → Select
     if not checkboxes[0].is_selected():
         checkboxes[0].click()
 
     assert checkboxes[0].is_selected()
 
+    # Second checkbox → Deselect
     if checkboxes[1].is_selected():
         checkboxes[1].click()
 
     assert not checkboxes[1].is_selected()
-
-    driver.quit()
