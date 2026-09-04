e552f8fded86c3f7435ba54bfc390ecee5be211a
Madeshwaran <madeshwaranponnudurai@gmail.com>
2026-09-04 19:48:06 +0530

    Refactor dropdown test to use pytest fixture
    Added a pytest fixture for the WebDriver and removed redundant driver.quit() call from the test function.

--- 14_selenium_dropdowns/test_dropdown.py
+++ 14_selenium_dropdowns/test_dropdown.py
@@ -1,15 +1,21 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
 
-def test_dropdown():
+@pytest.fixture
+def driver():
     driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+
+def test_dropdown(driver):
     driver.get("https://the-internet.herokuapp.com/dropdown")
 
     dropdown = Select(driver.find_element(By.ID, "dropdown"))
+
     dropdown.select_by_visible_text("Option 2")
 
     assert dropdown.first_selected_option.text == "Option 2"
-
-    driver.quit()
