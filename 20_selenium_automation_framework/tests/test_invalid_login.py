555ada51d2ca41fe4486ba059b191b307bbc553d
Madeshwaran <madeshwaranponnudurai@gmail.com>
2026-09-05 00:05:18 +0530

    Refactor test_invalid_login to use parameterization

--- 20_selenium_automation_framework/tests/test_invalid_login.py
+++ 20_selenium_automation_framework/tests/test_invalid_login.py
@@ -1,12 +1,32 @@
+import pytest
+
 from pages.login_page import LoginPage
-from utils.config_reader import get_config
 
 
-def test_invalid_login(driver):
-    config = get_config()
-    page = LoginPage(driver)
+@pytest.mark.parametrize(
+    "username,password,expected_message",
+    [
+        (
+            "tomsmith",
+            "wrong-password",
+            "Your password is invalid!"
+        ),
+        (
+            "wrong-user",
+            "SuperSecretPassword!",
+            "Your username is invalid!"
+        ),
+    ],
+)
+def test_invalid_login(
+    driver,
+    username,
+    password,
+    expected_message
+):
+    login_page = LoginPage(driver)
 
-    page.open(config["application"]["base_url"])
-    page.login("tomsmith", "wrong-password")
+    login_page.open()
+    login_page.login(username, password)
 
-    assert "Your password is invalid!" in page.get_message()
+    assert expected_message in login_page.get_flash_message()
