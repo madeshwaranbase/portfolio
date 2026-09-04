dd0f658a2d07df1ca4e3706f150becca275b845e
Madeshwaran <madeshwaranponnudurai@gmail.com>
2026-09-04 19:34:48 +0530

    Refactor login test to use pytest fixture
    Refactor test_login to use pytest fixture for driver setup and improve element wait handling.

--- 11_selenium_login_test/test_login.py
+++ 11_selenium_login_test/test_login.py
@@ -1,16 +1,28 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+LOGIN_URL = "https://the-internet.herokuapp.com/login"
 
 
-def test_login():
-    driver = webdriver.Chrome()
-    driver.get("https://the-internet.herokuapp.com/login")
+@pytest.fixture
+def driver():
+    drv = webdriver.Chrome()
+    drv.implicitly_wait(2)
+    yield drv
+    drv.quit()  # always runs, even if the test body fails/asserts
 
+
+def test_login(driver):
+    driver.get(LOGIN_URL)
     driver.find_element(By.ID, "username").send_keys("tomsmith")
     driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
     driver.find_element(By.CSS_SELECTOR, "button.radius").click()
 
-    message = driver.find_element(By.ID, "flash").text
-    assert "You logged into a secure area!" in message
+    flash = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.ID, "flash"))
+    )
 
-    driver.quit()
+    assert "You logged into a secure area!" in flash.text
