6c2d8c8fd3db1d74e56f11fd12eed7450f043773
Madeshwaran <madeshwaranponnudurai@gmail.com>
2026-09-05 00:04:55 +0530

    Refactor login test to use LoginPage methods

--- 20_selenium_automation_framework/tests/test_login.py
+++ 20_selenium_automation_framework/tests/test_login.py
@@ -1,22 +1,13 @@
 from pages.login_page import LoginPage
-from utils.config_reader import get_config
-from utils.logger import get_logger
-
-
-logger = get_logger(__name__)
 
 
 def test_valid_login(driver):
-    config = get_config()
-    page = LoginPage(driver)
+    login_page = LoginPage(driver)
 
-    logger.info("Opening login page")
-    page.open(config["application"]["base_url"])
+    login_page.open()
+    login_page.login(
+        "tomsmith",
+        "SuperSecretPassword!"
+    )
 
-    logger.info("Logging in with valid credentials")
-    page.login("tomsmith", "SuperSecretPassword!")
-
-    message = page.get_message()
-
-    assert "You logged into a secure area!" in message
-    logger.info("Login test passed")
+    assert "You logged into a secure area!" in login_page.get_flash_message()
