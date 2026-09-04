ab49c0f871d13cc54319d10c54043513ded8a71d
Madeshwaran <madeshwaranponnudurai@gmail.com>
2026-09-05 00:00:36 +0530

    Refactor LoginPage methods for better abstraction

--- 20_selenium_automation_framework/pages/login_page.py
+++ 20_selenium_automation_framework/pages/login_page.py
@@ -1,22 +1,22 @@
 from selenium.webdriver.common.by import By
 
+from pages.base_page import BasePage
 
-class LoginPage:
+
+class LoginPage(BasePage):
+
     USERNAME = (By.ID, "username")
     PASSWORD = (By.ID, "password")
     LOGIN_BUTTON = (By.CSS_SELECTOR, "button.radius")
     FLASH_MESSAGE = (By.ID, "flash")
 
-    def __init__(self, driver):
-        self.driver = driver
-
     def open(self, base_url):
         self.driver.get(f"{base_url}/login")
 
     def login(self, username, password):
-        self.driver.find_element(*self.USERNAME).send_keys(username)
-        self.driver.find_element(*self.PASSWORD).send_keys(password)
-        self.driver.find_element(*self.LOGIN_BUTTON).click()
+        self.enter_text(self.USERNAME, username)
+        self.enter_text(self.PASSWORD, password)
+        self.click(self.LOGIN_BUTTON)
 
-    def get_message(self):
-        return self.driver.find_element(*self.FLASH_MESSAGE).text
+    def get_flash_message(self):
+        return self.get_text(self.FLASH_MESSAGE)
