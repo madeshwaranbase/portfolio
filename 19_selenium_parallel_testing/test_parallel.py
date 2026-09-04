38ebeea8d956bb91c3480cd44dabbf49a6afe602
Madeshwaran <madeshwaranponnudurai@gmail.com>
2026-09-04 20:31:51 +0530

    Add try-finally to ensure driver quits
    Refactor tests to ensure driver quits in case of exceptions.

--- 19_selenium_parallel_testing/test_parallel.py
+++ 19_selenium_parallel_testing/test_parallel.py
@@ -3,20 +3,26 @@ from selenium import webdriver
 
 def test_home_page():
     driver = webdriver.Chrome()
-    driver.get("https://the-internet.herokuapp.com/")
-    assert "The Internet" in driver.title
-    driver.quit()
+    try:
+        driver.get("https://the-internet.herokuapp.com/")
+        assert "The Internet" in driver.title
+    finally:
+        driver.quit()
 
 
 def test_login_page():
     driver = webdriver.Chrome()
-    driver.get("https://the-internet.herokuapp.com/login")
-    assert "Login Page" in driver.title
-    driver.quit()
+    try:
+        driver.get("https://the-internet.herokuapp.com/login")
+        assert "Login Page" in driver.title
+    finally:
+        driver.quit()
 
 
 def test_checkbox_page():
     driver = webdriver.Chrome()
-    driver.get("https://the-internet.herokuapp.com/checkboxes")
-    assert "Checkboxes" in driver.title
-    driver.quit()
+    try:
+        driver.get("https://the-internet.herokuapp.com/checkboxes")
+        assert "Checkboxes" in driver.title
+    finally:
+        driver.quit()
